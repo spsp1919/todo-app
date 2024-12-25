801c53cceddefbe3537b4b0b2e562c5512f837d4
sushant <sushantp1927@gmail.com>
2024-12-25 21:40:16 +0530

    file compression program with functions

--- 155bonus.py
+++ 155bonus.py
@@ -1,15 +1,24 @@
 
-
 import FreeSimpleGUI as sg
+from zipfilefunction import  make_archive
 label1 =sg.Text("select files to compress")
 input1 =sg.Input()
-choose_button1 =sg.FilesBrowse("choose")
+
+choose_button1 =sg.FilesBrowse("choose",key="files")
 label2 =sg.Text("select destiantion folder")
 input2 =sg.Input()
-choose_button2 =sg.FolderBrowse("choose")
+
+choose_button2 =sg.FolderBrowse("choose",key="folder")
 compress_button =sg.Button("compress")
 
 window =sg.Window("file compressor",layout=[[label1,input1,choose_button1],
                                           [label2,input2,choose_button2 ],[compress_button]])
-window.read()
+
+while True:
+    event,values = window.read()
+    print(event,values)
+    filepaths = values["files"].split(";")
+    folder=values["folder"]
+    make_archive(filepaths,folder)
+
 window.close()
