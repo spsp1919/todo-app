82386f485ef6190dab5bad29bbad310dfbb11fb3
sushant <sushantp1927@gmail.com>
2024-12-27 10:54:16 +0530

    added complete and exit buttons

--- graphics.py
+++ graphics.py
@@ -2,35 +2,49 @@ import functions
 import FreeSimpleGUI as sg
 
 label=sg.Text("Type in a todo")
-input_box =sg.InputText(tooltip="Enter Todo",key='todo')
-add_button=sg.Button("Add")
+input_box =sg.InputText(tooltip="Enter Todo",key="todo")
+add_button=sg.Button("add")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',enable_events=True,
-                      size=[45,10])
-edit_button = sg.Button("Edit")
+                      size=(45,10))
+edit_button = sg.Button("edit")
+complete_button = sg.Button("complete")
+exit_button = sg.Button("exit")
 window=sg.Window("My Todo App",
-                 layout=[[label],[input_box,add_button],[list_box,edit_button]],
+                 layout=[[label],[input_box,add_button],
+                         [list_box,edit_button,complete_button],
+                         [exit_button]],
                 font=('Helvetica',20))
 while True:
     event,values =window.read()
     print(1,event)
     print(2,values)
-    print(3,values['todos'])
+
     match event:
-        case"Add":
+        case"add":
             todos = functions.get_todos()
-            new_todo = values['todo'] + "\n"
+            new_todo = values["todo"] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-        case"Edit":
+            window['todo'].update(value="")
+        case"edit":
             todo_edit=values['todos'][0]
-            new_todo = values['todo']
+            new_todo = values['todo'].strip()
             todos = functions.get_todos()
             index = todos.index(todo_edit)
-            todos[index] = new_todo
+            todos[index] = new_todo +"\n"
             functions.write_todos(todos)
             window['todos'].update(values=todos)
-
+            window['todo'].update(value="")
+        case "complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value=" ")
+        case "exit":
+              break
         case'todos':
             window['todo'].update(value=values['todos'][0])
 
