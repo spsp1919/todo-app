1a52dd16cd94ad7322cffd3ca4634cadfc1e65df
sushant <sushantp1927@gmail.com>
2024-12-28 10:41:48 +0530

    added import os module

--- graphics.py
+++ graphics.py
@@ -3,13 +3,18 @@
 import functions
 import FreeSimpleGUI as sg
 import time
+import os
+
+if not os.path.exists("todos.txt"):
+    with open("todos.txt",'w') as file:
+        pass
 
 sg.theme("BlackMono")
 clock=sg.Text(' ',key='clock')
 
 label=sg.Text("Type in a todo")
 input_box =sg.InputText(tooltip="Enter Todo",key="todo")
-add_button=sg.Button("add")
+add_button=sg.Button("add",tooltip="add todo")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',enable_events=True,
                       size=(45,10))
 edit_button = sg.Button("edit")
